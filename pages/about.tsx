f07d683a5a38d5d3ab3d69ddfebe03cf0fa24be6
Ilya Amelevich <ilya.amelevich@gmail.com>
2022-09-04 04:26:01 +0200

    Finish about page
    Signed-off-by: Ilya Amelevich <ilya.amelevich@gmail.com>

--- pages/about.tsx
+++ pages/about.tsx
@@ -114,6 +114,182 @@ export default function About({}) {
         <h2 className="text-2xl font-extrabold leading-tight mb-2 bg-gradient-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent">
           Employment History
         </h2>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            Senior Node.js/GoLang Engineer at Vitafy GmbH (Germany), Remote
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            March 2022 - present
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>Support and develop clod architecture</li>
+            <li>
+              Create new architecture of the E-commerce shops and implement it
+            </li>
+            <li>Support and develop CI/CD pipelines</li>
+            <li>Maintain some monorepos with Rush</li>
+          </ul>
+        </div>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            Senior Node.js/GoLang Engineer at PlutoTV (USA), Remote
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            December 2018 - March 2022
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>
+              Designed and implemented the analytic events delivery system using
+              GoLang and Kafka.
+            </li>
+            <li>Designed a logging system using Kafka and Vector.</li>
+            <li>
+              Made performance improvements for different micro-services using
+              Redis,
+            </li>
+            <li>MongoDB, and Kafka.</li>
+            <li>
+              Took part in moving from monolithic to microservice architecture.
+            </li>
+            <li>
+              Created and supported libraries for logging and configuration of
+              services (Node.js and GoLang).
+            </li>
+            <li>Helped with the configuration of CI/CD for some services.</li>
+            <li>Took part in an interview process.</li>
+          </ul>
+        </div>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            Node.js Mentor at foxminded (Ukraine), Remote
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            October 2021 - March 2022
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>Answer students questions and make a code review.</li>
+          </ul>
+        </div>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            R{'&'}D Engineer at Hola (Israel), Remote
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            July 2018 — December 2018
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>
+              Took part in the development of the Hola Phone application using
+              React Native and Node.js.
+            </li>
+            <li>
+              Implemented Speech-To-Text function for voice messages in the Hola
+              Phone application.
+            </li>
+            <li>
+              Made some performance improvements (including CPU/Memory usage
+              modal window for developer{"'"}s purposes).
+            </li>
+          </ul>
+        </div>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            Senior Node.js Engineer at Skywind Group (Belarus), Minsk
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            August 2017 — June 2018
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>Implemented back-end for a few Slot games.</li>
+            <li>Made unit tests of the Slot games with coverage 100%.</li>
+            <li>
+              Made tests of the Slot games to be the same as the math model for
+              it.
+            </li>
+          </ul>
+        </div>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            Fullstack Engineer at XB Software (Belarus), Minsk
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            August 2016 — August 2017
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>
+              Was responsible for the part of the main site of the application (
+              <a href="https://www.ceicdata.com/" className="gradient-link">
+                https://www.ceicdata.com/
+              </a>
+              ).
+            </li>
+            <li>
+              Utilized pure JavaScript to create the admin panel for marketing
+              specialists where, with the help of simple custom query language,
+              they can create templates for different pages with data from DB.
+            </li>
+            <li>
+              Made server rendering for charts to prevent stealing the data.
+            </li>
+            <li>Made integration with a payment gateway.</li>
+          </ul>
+        </div>
+        <div className="my-4">
+          <h3 className="text-lg font-extrabold leading-tight">
+            Fullstack Engineer at iKantam (Belarus), Minsk
+          </h3>
+          <p className="text-right text-sm text-slate-400">
+            May 2015 — August 2016
+          </p>
+          <ul className="list-disc text-left ml-10">
+            <li>Worked on some projects (Upwork).</li>
+            <li>
+              A system that automates social media management (
+              <a href="http://smintly.com/" className="gradient-link">
+                http://smintly.com/
+              </a>
+              ) (PHP + JS on frontend):
+            </li>
+            <ul className="list-disc text-left ml-10">
+              <li>
+                Made full refactoring of the system that allows a user to
+                leverage multiple accounts in social media.
+              </li>
+              <li>Updated frontend for the new design.</li>
+              <li>Created constructor for social media posts.</li>
+              <li>
+                Made a lot of different improvements including performance.
+              </li>
+              <li>
+                Admin panel with API that helps to split payment transactions
+                based on the predefined logic (Node.js, MongoDB, Angular) was
+                created from scratch in a few months only by me.
+              </li>
+            </ul>
+            <li>
+              Marketplace for cars in the USA (PHP + Angular). The customer was
+              also a developer so we worked together:
+            </li>
+            <ul className="list-disc text-left ml-10">
+              <li>Implemented a new design.</li>
+              <li>
+                A lot of updates in the DB structure and in back-end code.
+              </li>
+            </ul>
+            <li>
+              Service that allows you to use a Tesla as a taxi written in
+              Meteor.js:
+            </li>
+            <ul className="list-disc text-left ml-10">
+              <li>Some fixes in the order process.</li>
+            </ul>
+            <li>Marketplace for beauty goods (PHP + Angular):</li>
+            <ul className="list-disc text-left ml-10">
+              <li>Categories and Product pages.</li>
+              <li>Sorting and filtering of the categories and products.</li>
+            </ul>
+          </ul>
+        </div>
       </section>
     </Layout>
   );
